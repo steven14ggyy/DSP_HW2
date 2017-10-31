0a34eed4b1f02fec2f9d76e65ed0d73f44bd43bb
steven14ggyy <32742691+steven14ggyy@users.noreply.github.com>
2017-10-31 23:21:48 +0800

    Add files via upload

--- MyHarrisCornerDetector.m
+++ MyHarrisCornerDetector.m
@@ -50,14 +50,25 @@ ymin = 1;
 %%%%%%
 % get image gradient
 % [Your Code here] 
+%Transform into gray scale image
+I = I(:,:,1)*0.299+I(:,:,2)*0.587+I(:,:,3)*0.114;
 % calculate Ix
+Ix = imfilter(I, dx);
 % calcualte Iy
+Iy = imfilter(I, dy);
 %%%%%
+
 % get all components of second moment matrix M = [[Ix2 Ixy];[Iyx Iy2]]; note Ix2 Ixy Iy2 are all Gaussian smoothed
 % [Your Code here] 
 % calculate Ix2  
+Ix2 = Ix.*Ix;
+Ix2 = imfilter(Ix2, g);
 % calculate Iy2
+Iy2 =  Iy.*Iy;
+Iy2 = imfilter(Iy2, g);
 % calculate Ixy
+Ixy =  Ix.*Iy;
+Ixy = imfilter(Ixy, g);
 %%%%%
 
 %% visualize Ixy
@@ -66,11 +77,11 @@ imagesc(Ixy);
 
 %%%%%%% Demo Check Point -------------------
 
-
 %%%%%
 % get corner response function R = det(M)-alpha*trace(M)^2 
 % [Your Code here] 
 % calculate R
+R = Ix2.*Iy2-Ixy.*Ixy - alpha*(Ix2+Iy2).^2;
 %%%%%
 
 %% make max R value to be 1000
@@ -81,12 +92,22 @@ R=(1000/max(max(R)))*R; % be aware of if max(R) is 0 or not
 sze = 2*r+1; % domain width 
 % [Your Code here] 
 % calculate MX
+MX = ordfilt2(R,sze^2,ones(sze,sze));
 %%%%%
 
 %%%%%
 % find local maximum.
 % [Your Code here] 
 % calculate RBinary
+RBinary = zeros(xmax,ymax);
+for i = xmin+1:xmax-1
+    for j =  ymin+1:ymax-1
+        if(R(i,j)>Thrshold && R(i,j)==MX(i,j))
+            RBinary(i,j) = 1;
+        end
+    end
+    a=1;
+end
 %%%%%
 
 
@@ -100,7 +121,7 @@ R(offe:size(RBinary,1)-offe,offe:size(RBinary,2)-offe)=RBinary(offe:size(RBinary
 
 %% Display
 figure(3)
-imagesc(uint8(I));
+imagesc(uint8(frame));
 hold on;
 plot(c1,r1,'or');
 return;
